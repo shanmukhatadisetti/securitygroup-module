1f69e22291bc8ad7c9feaac876abd82e63f128bc
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-12-09 09:13:44 +0000

    security module development

--- sg.tf
+++ sg.tf
@@ -2,6 +2,14 @@ resource "aws_security_group" "main" {
   name        = var.sg_name
   description = var.description
   vpc_id      = var.vpc_id
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 
   tags = merge(
     var.sg_tags,
